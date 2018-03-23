ea39b81b14f530822ac31ecfadca07c881268a5a
andrewcgaitskellhs2 <36811435+andrewcgaitskellhs2@users.noreply.github.com>
2018-03-23 18:50:10 +0000

    Create node1.js

---
+++ node1/node1.js
@@ -0,0 +1,11 @@
+module.exports = function(RED) {
+    function Node1Node(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("node1",Node1Node);
+}
